063aad87a6f7a604de0643b079f504e1c858dfdb
Meiling <mcastillog@utem.cl>
2025-07-01 05:13:02 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@
 'use client'
 import { NextPage } from 'next'
 import React from 'react'
+import Cliente from '@/components/tresprimeras';
+
+ 
 
 const Home: NextPage = () => {
     return (
-        <div className="space-y-6 p-6 my-[58px]">
-            <h1>Base</h1>
+        <div className="space-y-6 p-6 my-[58px]"> 
+            <Cliente />                
         </div>
             )
-            }
+            
+}
 
 export default Home
