fe8904ce42cd5ae6c415ed58e3f22dd56eb85b82
JimNicolas <jimeneznicolas2318@hotmail.com>
2025-07-04 23:12:40 -0400

    Se modificó usuario cliente cotización y total
    se modificó usuario, cliente, cotización, productos y total de tal manera que ambos quedaran en el mismo contenedor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import Cliente from '@/components/tresprimeras';
 const Home: NextPage = () => {
     return (
         <div className="min-h-screen bg-gray-100">
-        <Cliente />    
             <div className="container mx-auto py-8 pt-[66px]">
+        <Cliente />
                 <QuotationTable quotationId="q1" />
             </div>
         </div>
