e150b4c58c2a0e861194c370384d65176f079f2e
Meiling <mcastillog@utem.cl>
2025-07-14 18:15:34 -0400

    Complete la parte visual de Cliente.tsx

--- src/components/Cliente.tsx
+++ src/components/Cliente.tsx
@@ -41,22 +41,63 @@ export default function Cliente() {
             </div>
         
 
-            {/*Modal*/}
+            {/*Modal boton ver historial*/}
             {MostrarModal_His && (
             <div className="fixed inset-0 flex justify-center items-center">
-            <div className="bg-[#0B1631] p-8 rounded-lg w-[600px]">
+              <div className="bg-[#0B1631] p-8 rounded-lg w-[900px]">
+
+                {/*Titulo Barra Icono*/}
+                <div className="flex">
                 <h2 className="text-2xl text-white font-semibold mb-4">Cotizaciones</h2>
+                {/*Icono barra*/}
+                <div  className="relative">
+                    <span className="absolute ml-5 top-[16] inset-y-1/2 left-3 items-center text-[#949494]">
+                        <FaSearch />
+                    </span>
+                </div>
+                {/*Barra busqueda*/}
+                <input 
+                type="text"
+                placeholder="Buscar"
+                className="ml-5 pl-10 px-3 py-1 border rounded-md bg-white border-[#DFDFDF] w-[570]"/>
+                </div>
 
-
+                {/*Tablas*/}
                 <div className="flex my-4">
 
-                    <div className="rounded bg-white w-[250]">
-                    <p className="mb-4">prueba</p>
+                    <div className="center w-[900]">
+                        
+                        <table className=" w-full text-left border border-gray-300 rounded-md">
+                            <thead>
+                                <tr className="text-center bg-gray-100">
+                                    <th className="border-t p-2">ID</th>
+                                    <th className="border-t p-2">Nombre</th>
+                                    <th className="border-t p-2">Fecha</th>
+                                    <th className="border-t p-2">Cantidad</th>
+                                    <th className="border-t p-2">Total</th>
+                                    <th className="border-t p-2">Estado</th>
+                                    <th className="border-t p-2">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="text-center bg-white">
+                                    <td className="border-t p-2">101</td>
+                                    <td className="border-t p-2">Cotización01</td>
+                                    <td className="border-t p-2">10-01-25</td>
+                                    <td className="border-t p-2">12</td>
+                                    <td className="border-t p-2">24.500</td>
+                                    <td className="border-t p-2">pendiente</td>
+                                    <td className="border-t p-2"></td>
+                                </tr>
+                        
+                             </tbody>
+                        </table>
+                        
                     </div>
 
                 </div>
 
-
+                {/*Boton para cerrar modal*/}
                 <div className="flex justify-end">
                 <button
                     className="px-17 py-2 border border-white bg-[#0B1631] text-white rounded hover:bg-[#15295C]"
@@ -66,33 +107,136 @@ export default function Cliente() {
                 </button>
                 </div>
 
-            </div>
+              </div>
             </div>
             )}
             
-
+            {/*Boton añadir cliente*/}
             {AñadirCliente && (
             <div className="fixed inset-0 flex justify-center items-center">
             <div className="bg-[#0B1631] p-8 rounded-lg w-[600px]">
-                <h2 className="text-2xl text-white font-semibold mb-4">Cliente</h2>
+                {/*Titulo*/}
+                <h2 className="text-3xl text-white font-semibold mb-2">Cliente</h2>
 
-                    <div className="rounded bg-white w-[200]">
-                    <p className="mb-4">Nombre</p>
+                {/*Titulos con flex y boton*/}
+                <div className="flex font-bold text-white text-xl my-2">
+                    <p className="ml-1 mb-1 mt-2">Datos cliente</p>
+                    <p className="ml-37 mb-1 mt-2">Direcciones</p>
+                    {/*Boton más*/}
+                    <button className="w-7 h-7 mt-2 ml-2 items-center gap-3 text-white rounded-full justify-center 
+                            text-xl font-bold bg-[#4CAF50] hover:bg-[#3E8F41] cursor-pointer"
+                            onClick={() => []}>
+                    +
+                    </button>
+                </div>
+
+                {/*Dos tablas con flex*/}
+                <div className="flex justify-center mb-5">
+
+                    {/*Tabla uno*/}
+                    <div className="rounded bg-white w-[300]">
+                        <p className="ml-3 mb-1 mt-2 font-bold">Nombre</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: Nicolás Jiménez"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input> 
+
+                        <p className="ml-3 mb-1 mt-1 font-bold">Tipo de cliente</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: Nicolás Jiménez"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+
+                        <p className="ml-3 mb-1 mt-1 font-bold">Teléfono</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: +56912345678"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+
+                        <p className="ml-3 mb-1 mt-1 font-bold">Email</p>
+                        <input
+                            type="email"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: nicolas@correo.cl"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+
                     </div>
 
+                    {/*Tabla dos*/}
+                    <div className="ml-4 rounded bg-white w-[300]">
+
+                        <p className="ml-3 mb-1 mt-2 font-bold">Nombre</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: Casa"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input> 
+
+                        <p className="ml-3 mb-1 mt-1 font-bold">Dirección principal</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: calle #1234"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+
+                        <p className="ml-3 mb-1 mt-1 font-bold">Comuna</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: +56912345678"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+
+                        <p className="ml-3 mb-1 mt-1 font-bold">Ciudad</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: nicolas@correo.cl"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+                        
+                        <p className="ml-3 mb-1 mt-1 font-bold">País</p>
+                        <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="ej: Chile"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input>  
+
+                    </div>
+                </div>
+                {/*Botones Cancelar y guardar*/}
                 <div className="flex justify-end">
                 <button
-                    className="px-17 py-2 border border-white bg-[#0B1631] text-white rounded hover:bg-[#15295C]"
+                    className="px-25 py-2 border border-white bg-[#0B1631] text-white rounded hover:bg-[#15295C]"
                     onClick={() => setAñadirCliente(false)}
                 >
                     Cancelar
                 </button>
-                 <button
-                    className="px-17 py-2 ml-2 bg-[#F59243] text-white rounded hover:bg-[#FF9243]"
+                <button
+                    className="px-25 py-2 ml-3 bg-[#F59243] text-white rounded hover:bg-[#FF9243]"
                     onClick={() => setAñadirCliente(false)}
                 >
                     Guardar
-                                </button>
+                </button>
                 </div>
             </div>
             </div>
@@ -101,15 +245,17 @@ export default function Cliente() {
             {mostrarModal && (
             <div className="fixed inset-0 flex justify-center items-center">
             <div className="bg-[#0B1631] p-8 rounded-lg w-[650px]">
+
+                {/*Titulo*/}
                 <h2 className="text-2xl text-white font-semibold mb-4">Detalle cotización</h2>
 
-
+                {/*Dos tablas con flex*/}
                 <div className="flex my-4">
 
+                    {/*Tabla uno*/}
                     <div className="rounded bg-white w-[325]">
 
-
-                    <p className="ml-3 mb-1 mt-2">Nombre</p>
+                    <p className="ml-3 mb-1 mt-2 font-bold">Nombre</p>
                         <input
                             type="text"
                             /*value={}*/
@@ -118,7 +264,7 @@ export default function Cliente() {
                             className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[290]"
                         ></input>      
 
-                    <p className="ml-3 mb-1">Id</p>
+                    <p className="ml-3 mb-1 font-bold">Id</p>
                         <input
                             type="text"
                             /*value={}*/
@@ -127,7 +273,7 @@ export default function Cliente() {
                             className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[290]"
                         ></input> 
 
-                    <p className="ml-3 mb-1">Descripción</p>
+                    <p className="ml-3 mb-1 font-bold">Descripción</p>
                         <input
                             type="text"
                             /*value={}*/
@@ -137,8 +283,8 @@ export default function Cliente() {
                         ></input> 
                     
                     <div className="flex">
-                     <p className="ml-3 mb-1">Fecha creación</p>
-                     <p className="ml-5 mb-1">Vigencia</p>
+                     <p className="ml-3 mb-1 font-bold">Fecha creación</p>
+                     <p className="ml-11 mb-1 font-bold">Vigencia</p>
                      </div>
                     <div className="flex">
                        
@@ -158,7 +304,7 @@ export default function Cliente() {
                         ></input> 
                     </div>
 
-                    <p className="ml-3 mb-1">Dirección destino</p>
+                    <p className="ml-3 mb-1 font-bold">Dirección destino</p>
                         <input
                             type="text"
                             /*value={}*/
@@ -167,7 +313,7 @@ export default function Cliente() {
                             className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[290]"
                         ></input> 
 
-                    <p className="ml-3 mb-1">Creado por</p>
+                    <p className="ml-3 mb-1 font-bold">Creado por</p>
                         <input
                             type="text"
                             /*value={}*/
@@ -176,7 +322,7 @@ export default function Cliente() {
                             className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[290]"
                         ></input> 
 
-                    <p className="ml-3 mb-1">Total</p>
+                    <p className="ml-3 mb-1 font-bold">Total</p>
                         <input
                             type="number"
                             /*value={}*/
@@ -187,22 +333,64 @@ export default function Cliente() {
 
                     </div>
 
-                    <div className="ml-4 rounded bg-white w-[269]">
-                    <p className="mb-4">Cliente</p>
+
+                {/*Tabla dos*/}
+                    <div className="ml-4 rounded bg-white h-[380] w-[269]">
+
+                    <p className="mt-2 ml-2 mb-1 font-bold">Cliente</p>
+                                            <input
+                            type="text"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="Cliente"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input> 
+
+                    <p className="ml-2 mb-1 font-bold">Email</p>
+                                            <input
+                            type="email"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="Email@Example.cl"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input> 
+
+                    <p className="ml-2 mb-1 font-bold">Teléfono</p>
+                                            <input
+                            type="email"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="+56912345678"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input> 
+
+                    <p className="ml-2 mb-1 font-bold">Tipo cliente</p>
+                                            <input
+                            type="email"
+                            /*value={}*/
+                            onChange={() => {}}
+                            placeholder="tipo"
+                            className="ml-2 mb-2 border rounded-sm border-[#DFDFDF] px-3 py-1 w-[240]"
+                        ></input> 
+
+                    <button className="my-2 ml-2 px-16 py-4 mr-2 text-2xl text-black font-bold rounded bg-[#F1F6EF]">
+                        Aprobada
+                    </button>
+
                     </div>
 
                 </div>
 
-
+                {/*Botones Cancelar y guardar*/}
                 <div className="flex justify-end">
                 <button
-                    className="px-17 py-2 border border-white bg-[#0B1631] text-white rounded hover:bg-[#15295C]"
+                    className="px-28 py-2 border border-white bg-[#0B1631] text-white rounded hover:bg-[#15295C]"
                     onClick={() => setMostrarModal(false)}
                 >
                     Cancelar
                 </button>
                  <button
-                    className="px-17 py-2 ml-2 bg-[#F59243] text-white rounded hover:bg-[#FF9243]"
+                    className="px-28 py-2 ml-3 bg-[#F59243] text-white rounded hover:bg-[#FF9243]"
                     onClick={() => setMostrarModal(false)}
                 >
                     Guardar
