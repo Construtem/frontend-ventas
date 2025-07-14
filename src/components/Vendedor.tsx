ac1adc7cd6fb2eb69ba24d5490e62753061140e8
Meiling <mcastillog@utem.cl>
2025-07-13 23:21:50 -0400

    Add files via upload
    La parte solo estetica

---
+++ src/components/Vendedor.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+
+export default function Vendedor() {
+
+    return (
+        <div className="bg-white border border-gray-300 p-4 mx-auto rounded-lg w-[500px] ml-30 mb-4">
+
+            <div className="ml-3">
+            <div className="text-lg font-bold text-black flex gap-2 mb-2">
+                Nombre Vendedor
+            </div>
+
+            <div className="text-base text-black font-bold flex">
+                Rut 20.474.207-3 Nombre tienda
+            </div>
+            </div>
+            
+        </div>
+    );
+}
