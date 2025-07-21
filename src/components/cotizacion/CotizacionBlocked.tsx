be5a5d195a65798aeb5e7bc8c7a91de1f971c775
JimNicolas <jimeneznicolas2318@hotmail.com>
2025-07-21 17:24:50 -0400

    Se añadió el ícono 2 en la card cuando no hay clientes seleccionados

--- src/components/cotizacion/CotizacionBlocked.tsx
+++ src/components/cotizacion/CotizacionBlocked.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image';
 import QuoteIcon from '@/styles/images/QuoteIcon.png';
 export default function CotizacionBlocked() {
 
-    return (
-        <div className={'flex flex-col h-full justify-center'}>
-            <div className="flex flex-col  h-full  items-center gap-[5px]">
+    return (<>
+        <div className={'flex items-center flex-col h-full justify-center'}>
+            <div className="my-auto h-fit flex flex-col items-center gap-[5px]">
             <Image src={QuoteIcon} alt={'Icono de cotización'} width={200} height={200}/>
             <div className={'w-84'}>
             <h2 className={'text-center text-[#033c86] font-semibold font-montserrat text-[24px]'}>Primero selecciona un cliente para ver o gestionar cotizaciones</h2>
             </div>
             </div>
         </div>
+    </>
     )
 }
