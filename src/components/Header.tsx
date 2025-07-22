112ef2c8329e4907c5c00115d7f03874b19f85d1
Andres <andres.gomez.rodriguez2003@gmail.com>
2025-07-21 20:57:40 -0400

    Quitando proteccion de la ruta por el momento

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,9 +27,6 @@ const Header: React.FC = () => {
                 console.error("Error al parsear datos de usuario, limpiando localStorage:", err);
                 localStorage.removeItem("user");
             }
-        } else {
-            console.log("No se encontró información de usuario en localStorage, redirigiendo a login");
-            window.location.href = `${frontLoginUrl}`;
         }
     }, []);
 
